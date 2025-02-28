0027c8338bcf8d7e61b7c15a3b4f0f9115d77796
reiffle <pederreiff@gmail.com>
2025-02-28 12:29:22 -0600

    Added line-drawing

--- graphics.py
+++ graphics.py
@@ -20,3 +20,19 @@ class Window:
 
     def close(self):
         self.running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x=x
+        self.y=y
+
+class Line:
+    def __init__(self, point1=Point, point2=Point):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win=Window(800, 600)
+    win.draw_line(Line(Point(100, 100), Point(200, 200)))
+    win.draw_line(Line(Point(200, 200), Point(300, 100)))
+    win.draw_line(Line(Point(300, 100), Point(100, 100)))
     win.wait_for_close()
 
 main()
